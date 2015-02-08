6d9fe56d85f58c44ab42a0e6a506d1d7278b7d0b
Jason D Yeomans <jyeoman2@gmu.edu>
2015-02-08 00:57:46 -0500

    shit

--- django_project/api/urls.py
+++ django_project/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from api import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<tag_uuid>\d+)/$', views.process, name='process'),
     url(r'^whoshome/$', views.whoshome, name='whoshome'),
+    url(r'^(?P<tag_uuid>\d+)/$', views.process, name='process'),
 )
