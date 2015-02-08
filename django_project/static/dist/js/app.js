7b84357b28ef07f549979b270cb573f1e054a98d
Jason D Yeomans <jyeoman2@gmu.edu>
2015-02-08 07:57:36 -0500

    app.js

--- django_project/static/dist/js/app.js
+++ django_project/static/dist/js/app.js
@@ -1,11 +1,3 @@
-var app = angular.module("whoshome", []);
-
-app.config(function($httpProvider, $interpolateProvider) {
-  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  $interpolateProvider.startSymbol('{$');
-  $interpolateProvider.endSymbol('$}');
-});
-
 function update_table() {
   console.log("THIS FUNC RUNNING");
   $.ajax({
@@ -16,22 +8,3 @@ function update_table() {
     }
   });
 }
-
-app.controller("usersAtHomeCtrl", function($scope, $http) {
-
-  });
-  // $http.get('http://ip.jsontest.com/?callback=showMyIP').
-  //  success(function(data, status, headers, config) {
-  //    // this callback will be called asynchronously
-  //    // when the response is available
-
-  //    $scope.users = data;
-  //  }).
-  //  error(function(data, status, headers, config) {
-  //    // called asynchronously if an error occurs
-  //    // or server returns response with an error status.
-
-  //    $scope.users = "not good";
-  //  });
-
-});
