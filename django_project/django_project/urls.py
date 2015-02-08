821352e74048f52af11a36cd6dc11c316678866e
Jason D Yeomans <jyeoman2@gmu.edu>
2015-02-08 08:40:01 -0500

    urls

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from .views import IndexViews
 
 from django.contrib import admin
 admin.autodiscover()
